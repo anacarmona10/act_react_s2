7ed7d490f487073c678465ed6e25f3493e789789
anacarmona10 <anacarmona1023@gmail.com>
2025-09-18 10:17:28 -0500

    Agregando estilos a tarjeta modulo e implementandola

--- src/app/components/TarjetaModulo.js
+++ src/app/components/TarjetaModulo.js
@@ -1,11 +1,15 @@
 import React from "react";
+import styles from './TarjetaModulo.module.css'
+
 
 export default function TarjetaModulo({nombre, descripcion, categoria}) {
     return(
-        <>
-            <h3 className="nombre-modulo">{nombre}</h3>
-            <p className="descripcion">{descripcion}</p>
-            <div className="categoria">{categoria}</div>
-        </>
+        <article className={styles.tarjeta} >
+            <div className={styles.encabezado}>
+            <h3 className={styles.titulo}>{nombre}</h3>
+            </div>
+            <p className={styles.descripcion}>{descripcion}</p>
+            <div className={styles.categoria}>{categoria}</div>
+        </article>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 import TarjetaGlobal from './components/TarjetaGlobal';
 
+import TarjetaModulo from './components/TarjetaModulo'
+
 import React from 'react'
 
 export default function page() {
   return (
+    <>
     <TarjetaGlobal titulo={"Actividad 2"} contenido={"aserfgyuyghui"} color={'#A0C0DF'}/>
+    <TarjetaModulo nombre={"Tarjeta Modular"} descripcion={"fdgshjk"} categoria={"Músical"}/>
+    </>
   )
 }
 
